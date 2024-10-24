4973b4c3add598dbc91fe6e5b6f6ac3e789382a2
Jan Henckens <jan@statik.be>
2024-10-24 20:12:45 +0200

    Settings template & config example

---
+++ src/models/Settings.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace studioespresso\flare\models;
+
+use craft\base\Model;
+
+/**
+ * Flare settings
+ */
+class Settings extends Model
+{
+    public bool $enabled = true;
+
+    public ?string $apiKey = null;
+
+    public array $excludedExceptions = [];
+}
