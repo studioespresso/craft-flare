7b3ebd5a7d9fab3b46aff9b09ae6b54f8c9d91c6
Jan Henckens <jan@statik.be>
2024-11-08 19:51:21 +0100

    Settings fix & anonymizeIp

--- src/services/FlareService.php
+++ src/services/FlareService.php
@@ -4,33 +4,50 @@ namespace studioespressp\flare\services;
 
 use Craft;
 use craft\base\Component;
+use craft\helpers\App;
 use Spatie\FlareClient\Flare as FlareClient;
 use studioespresso\flare\Flare;
+use studioespresso\flare\models\Settings as SettingsModel;
 
 class FlareService extends Component
 {
+    /**
+     * @var FlareClient|null
+     */
     public ?FlareClient $client = null;
+
+    /**
+     * @var SettingsModel|null
+     */
+    private ?SettingsModel $settings = null;
 
     public function init(): void
     {
+        $this->settings = Flare::getInstance()->getSettings();
+
+
         if (
-            Flare::getInstance()->getSettings()->apiKey &&
-            Flare::getInstance()->getSettings()->enabled
+            $this->settings->apiKey &&
+            $this->settings->enabled
         ) {
-            $this->client = FlareClient::make(Flare::getInstance()->getSettings()->apiKey)->registerFlareHandlers();
+            $this->client = FlareClient::make(App::parseEnv($this->settings->apiKey))->registerFlareHandlers();
         }
         parent::init();
     }
 
     public function reportException(\Throwable $exception)
     {
-        $settings = Flare::getInstance()->getSettings();
-        if (in_array(get_class($exception), $settings->excludedExceptions)) {
+        if (in_array(get_class($exception), $this->settings->excludedExceptions, true)) {
             Craft::info('Exception class excluded from being reported to Flare.', Flare::getInstance()->handle);
             return;
         }
 
         if ($this->client) {
+
+            if ($this->settings->anonymizeIp) {
+                $this->client->anonymizeIp();
+            }
+
             $this->client->context('CMS', 'Craft CMS');
             $this->client->context('System name', Craft::$app->getSystemName());
             $this->client->context('Craft version', Craft::$app->getInfo()->version);
